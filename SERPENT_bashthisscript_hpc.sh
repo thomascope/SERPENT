19c952971fdf5624febb172c67d109b8c8fbcfa0
Thomas Cope <tec31@medschl.cam.ac.uk>
2019-03-27 20:11:17 +0000

    added freesurfer to pipeline

--- SERPENT_bashthisscript_hpc.sh
+++ SERPENT_bashthisscript_hpc.sh
@@ -228,5 +228,29 @@ if [ "$job_state" == "FAILED" ]
 fi
 done
 
+jobIDs=""
+prevstep=raw_nocopy
+step=freesurfer_hires
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
 cd ..
 
