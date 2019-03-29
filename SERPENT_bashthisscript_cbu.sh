2fc52046a1da2804286fbcb73d875da98c8d640d
Thomas Cope <tec31@medschl.cam.ac.uk>
2019-03-29 16:41:48 +0000

    CBU bash script

---
+++ SERPENT_bashthisscript_cbu.sh
@@ -0,0 +1,256 @@
+myscriptdir=/group/language/data/thomascope/7T_SERPENT_pilot_analysis
+clusterid=CBU
+
+submit=${myscriptdir}/SERPENT_submit_cbu.sh
+prepare=${myscriptdir}/SERPENT_prepare.sh
+func=${myscriptdir}/Preprocessing_mainfunction_7T.m
+subjs_def=${myscriptdir}/SERPENT_subjects_parameters.m
+
+#! declare -a steporder=("raw" "skullstrip" "realign" "topup" "cat12")
+#! subjects_to_process=($(seq 1 1 10))
+subjects_to_process=($(seq 14 1 25))
+
+prevstep=raw
+step=skullstrip
+
+jobIDs=""
+
+cd slurmoutputs
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=skullstrip
+step=realign
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=realign
+step=topup
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=topup
+step=reslice
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=topup
+step=cat12
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=topup
+step=coregister
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=topup
+step=normalisesmooth
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=smooth8
+step=SPM_uni
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=smooth3
+step=SPM_uni
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+jobIDs=""
+prevstep=raw_nocopy
+step=freesurfer_hires
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
+cd ..
+
