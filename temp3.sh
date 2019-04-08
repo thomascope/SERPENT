e68a5705f6f44c724648214cc59b597e1667e189
Thomas Cope <tec31@medschl.cam.ac.uk>
2019-04-08 15:54:41 +0100

    test

--- temp3.sh
+++ temp3.sh
@@ -36,5 +36,32 @@ if [ "$job_state" == "FAILED" ]
 fi
 done
 
+prevstep=raw_nocopy
+step=fmriprep
+
+submit=${myscriptdir}/SERPENT_submit_hphi_long.sh
+
+jobIDs=""
+
+for this_subj in ${subjects_to_process[@]}
+do 
+this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
+jobIDs="$jobIDs $this_job_id"
+done
+
+echo "submitted job stage" ${step} "polling for output before moving on"
+${myscriptdir}/waitForSlurmJobs.pl 1 10 $jobIDs
+for this_job in ${jobIDs[@]}
+do
+this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+job_state=`echo $this_job_outcome | awk '{print $1}'` 
+if [ "$job_state" == "FAILED" ] 
+    then
+        echo "SLURM submission failed - jobs went into error state"
+        exit 1;
+fi
+done
+
 cd ..
 
