32db8a55f01f93eba12321483925af3f2dc7f161
Thomas Cope <tec31@medschl.cam.ac.uk>
2019-04-09 12:33:12 +0100

    added slice encoding direction to json

--- temp4.sh
+++ temp4.sh
@@ -8,7 +8,7 @@ subjs_def=${myscriptdir}/SERPENT_subjects_parameters.m
 
 #! declare -a steporder=("raw" "skullstrip" "realign" "topup" "cat12")
 #! subjects_to_process=($(seq 1 1 10))
-subjects_to_process=($(seq 20 1 20))
+subjects_to_process=($(seq 5 1 5))
 
 prevstep=raw_nocopy
 step=fmriprep
